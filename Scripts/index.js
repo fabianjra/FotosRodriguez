2f52f0e15d3910a23659391533bb8ac18f2466c1
Fabian Rodriguez <fabianjra@hotmail.com>
2019-11-17 19:17:31 -0600

    Se cambia el diseó de las imagenes de inicio. Se agrega el footer

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
-    $(function () {
-       
-    });
-
 });
+
+function clickBotonNavbar() {
+
+    //ToggleClass agrega la clase si no la tiene, y si ya la tiene la remueve (como un tipo interruptor)
+    $(".navbar").toggleClass("colorNegro");
+};
