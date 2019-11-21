3b91cc2579675fb3645ee7702d7ce03a0d2f48c9
Fabian Rodriguez <fabianjra@hotmail.com>
2019-11-20 22:30:45 -0600

    Se agrega la pagina de contacto (en construccion)

---
+++ Scripts/global.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    $('#agregarMasterNavbar').load('MasterNavbar.html');
+    $('#agregarMasterFooter').load('MasterFooter.html');
+
+    // document.onload = AsignarPaginaActual();
+
+    var elemento = document.getElementById("navbarItemInicio");
+    console.log(elemento);
+});
+
+//ToggleClass agrega la clase si no la tiene, y si ya la tiene la remueve (como un tipo interruptor)
+function clickBotonNavbar() {
+    $(".navbar").toggleClass("colorNegro");
+};
+
+//Lee las variables contenidas en el "GET URL", y las retorna en un arreglo
+function GetQueryString() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+};
+
+function AsignarPaginaActual() {
+
+    var ActualID = GetQueryString()["p"];
+
+    switch (ActualID) {
+        case '0':
+            var elemento = document.getElementById("navbarItemInicio");
+            console.log(elemento);
+            elemento.classList.add("active");
+            break;
+
+        case '1':
+            var elemento = document.getElementById("navbarItemContacto");
+            elemento.classList.add("active");
+            break;
+
+        default:
+            var elemento = document.getElementById("navbarItemInicio");
+            console.log(elemento);
+            elemento.classList.add("active");
+            break;
+    }
+};
--- Scripts/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-});
-
-function clickBotonNavbar() {
-
-    //ToggleClass agrega la clase si no la tiene, y si ya la tiene la remueve (como un tipo interruptor)
-    $(".navbar").toggleClass("colorNegro");
-};
