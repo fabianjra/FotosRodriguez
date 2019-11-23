5cee849ba027ad65c7c0bd9486465cf02fb6d0d3
Fabian Rodriguez <fabianjra@hotmail.com>
2019-11-22 21:30:26 -0600

    Se quita el querystring

--- Scripts/global.js
+++ Scripts/global.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
-    $('#agregarMasterNavbar').load('MasterNavbar.html', AsignarPaginaActual);
+    // $('#agregarMasterNavbar').load('MasterNavbar.html', AsignarPaginaActual);
 
+    $('#agregarMasterNavbar').load('MasterNavbar.html');
     $('#agregarMasterFooter').load('MasterFooter.html');
 });
 
@@ -10,37 +11,50 @@ function clickBotonNavbar() {
     $(".navbar").toggleClass("colorNegro");
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Lee las variables contenidas en el "GET URL", y las retorna en un arreglo
-function GetQueryString() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+// function GetQueryString() {
+//     var vars = [], hash;
+//     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 
-    for (var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-};
+//     for (var i = 0; i < hashes.length; i++) {
+//         hash = hashes[i].split('=');
+//         vars.push(hash[0]);
+//         vars[hash[0]] = hash[1];
+//     }
+//     return vars;
+// };
 
-function AsignarPaginaActual() {
+// function AsignarPaginaActual() {
 
-    var ActualID = GetQueryString()["p"];
+//     var ActualID = GetQueryString()["p"];
 
-    switch (ActualID) {
-        case '0':
-            var elemento = document.getElementById("navbarItemInicio");
-            elemento.classList.add("active");
-            break;
+//     switch (ActualID) {
+//         case '0':
+//             var elemento = document.getElementById("navbarItemInicio");
+//             elemento.classList.add("active");
+//             break;
 
-        case '1':
-            var elemento = document.getElementById("navbarItemContacto");
-            elemento.classList.add("active");
-            break;
+//         case '1':
+//             var elemento = document.getElementById("navbarItemContacto");
+//             elemento.classList.add("active");
+//             break;
 
-        default:
-            var elemento = document.getElementById("navbarItemInicio");
-            elemento.classList.add("active");
-            break;
-    }
-};
+//         default:
+//             var elemento = document.getElementById("navbarItemInicio");
+//             elemento.classList.add("active");
+//             break;
+//     }
+// };
